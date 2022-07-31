af39f8096956d582565f3d15a36462224b3d8f6b
Priya6979 <bijaypriya5@gmail.com>
2022-07-31 09:00:35 +0530

    Initial commit

---
+++ initial/src/App.js
@@ -0,0 +1,15 @@
+import "./Style.css";
+import Inputforms from "./Inputforms";
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Inputforms />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ initial/src/Image.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Image = () => {
+  return (
+    <img
+      src="https://nafezly-production.s3.eu-west-3.amazonaws.com/uploads/services/16778_619d720721efb-1637708295.jpg"
+      className="img"
+      preview={false}
+      alt="banner"
+    ></img>
+  );
+};
+
+export default Image;
